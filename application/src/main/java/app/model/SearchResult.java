31927c649e997dbc4b92907e0851527a44372f30
raluca_adam-admin <raluca@adamii.ro>
2026-03-16 19:06:42 +0200

    feat(model): add SearchResult data class

---
+++ application/src/main/java/app/model/SearchResult.java
@@ -0,0 +1,12 @@
+package app.model;
+
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+
+public record SearchResult(
+        Path path,
+        String filename,
+        String extension,
+        String preview,
+        LocalDateTime modifiedAt
+) {}
