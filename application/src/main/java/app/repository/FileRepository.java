358cf42a363dd50f89b8ef7da0339a73462ad0b4
raluca_adam-admin <raluca@adamii.ro>
2026-03-21 13:32:35 +0200

    feat(repository): add FileRepository interface for database abstraction

---
+++ application/src/main/java/app/repository/FileRepository.java
@@ -0,0 +1,22 @@
+package app.repository;
+
+import app.model.FileRecord;
+import app.model.SearchResult;
+import app.search.query.Query;
+
+import java.nio.file.Path;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+public interface FileRepository {
+    void upsert(FileRecord record, String content, String preview) throws SQLException;
+    void delete(Path path) throws SQLException;
+    int batchDelete(Set<Path> paths) throws SQLException;
+    List<SearchResult> search(Query query) throws SQLException;
+    LocalDateTime getModifiedAt(Path path) throws SQLException;
+    FileRecord getByPath(Path path) throws SQLException;
+    List<FileRecord> getAll() throws SQLException;
+    List<FileRecord> getByExtension(String extension) throws SQLException;
+}
